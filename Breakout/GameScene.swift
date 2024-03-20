cff989a538d0339d389c5db6c56c565f9a4df967
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2024-03-19 22:05:15 -0500

    Created brick object

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -12,6 +12,7 @@ class GameScene: SKScene {
     
     var ball = SKShapeNode()
     var paddle = SKSpriteNode()
+    var brick = SKSpriteNode()
     
     override func didMove(to view: SKView) {
        // this stuff happens once(when the app opens)
@@ -23,6 +24,7 @@ class GameScene: SKScene {
         // this stuff happens before each game starts
         makeBall()
         makePaddle()
+        makeBrick()
     }
     
     func createBackground() {
@@ -75,6 +77,16 @@ class GameScene: SKScene {
         paddle.physicsBody = SKPhysicsBody(rectangleOf: paddle.size)
         paddle.physicsBody?.isDynamic = false
         addChild(paddle)
+    }
+    
+    func makeBrick() {
+        brick.removeFromParent() // remove the brick, if it exists
+        brick = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 20))
+        brick.position = CGPoint(x: frame.midX, y: frame.maxY - 50)
+        brick.name = "brick"
+        brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
+        brick.physicsBody?.isDynamic = false
+        addChild(brick)
     }
 }
     
