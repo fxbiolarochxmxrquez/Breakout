ca7ec4ab1b5eba00e1e1d58e923f9dcdf0132705
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2024-03-19 22:23:46 -0500

    Created paddle dynamic behavior

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -25,6 +25,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         kickBall()
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            paddle.position.x = location.x
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            paddle.position.x = location.x
+        }
+    }
+    
     func resetGame() {
         // this stuff happens before each game starts
         makeBall()
